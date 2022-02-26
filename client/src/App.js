fe784f18eead4a0ca22768ba7c3b555e6d83e745
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-02-26 05:21:46 -0800

    feat: toggle complete tasks function

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,31 @@ const App = () => {
   const [taskCount, setTaskCount] = useState(0);
   const [tasks, setTasks] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [text,setText] = useState('')
+  const [text, setText] = useState("");
   const address = "0x503BB3a1730f91F674970D8cA18Ef8d0b6EbC70C";
-  const todos = []
+  const todos = [];
 
   const addNewTask = (content) => {
-    setLoading(true)
+    setLoading(true);
     const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
     const todoList = new web3.eth.Contract(TodoList.abi, address);
     todoList.methods
       .createTask(content)
       .send({ from: account })
       .once("receipt", (receipt) => {
-        setLoading(false)
+        setLoading(false);
+      });
+  };
+
+  const toggleCompleteTask = (id) => {
+    setLoading(true);
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    const todoList = new web3.eth.Contract(TodoList.abi, address);
+    todoList.methods
+      .toggleCompleted(id)
+      .send({ from: account })
+      .once("receipt", (receipt) => {
+        setLoading(false);
       });
   };
 
@@ -33,11 +45,11 @@ const App = () => {
     const tasksContainer = [];
     for (var i = 1; i <= taskCount; i++) {
       const task = await todoList.methods.tasks(i).call();
-      tasksContainer.push(task)
+      tasksContainer.push(task);
       setTasks(tasksContainer);
     }
     setLoading(false);
-  };  
+  };
 
   useEffect(() => {
     loadBlockchainData();
@@ -50,17 +62,27 @@ const App = () => {
           <div>loading..</div>
         ) : (
           <>
-            <form onSubmit={e => {
-              e.preventDefault();
-              addNewTask(text);
-            }}>
-              <input type={"text"} onChange={e => setText(e.target.value)} placeholder="todo..." />
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                addNewTask(text);
+              }}
+            >
+              <input
+                type={"text"}
+                onChange={(e) => setText(e.target.value)}
+                placeholder="todo..."
+              />
               <button> Submit </button>
             </form>
             {tasks.map((task) => (
               <div key={task.id}>
                 <label>{task.content}</label>
-                <input type={"checkbox"} />
+                <input
+                  type={"checkbox"}
+                  onClick={() => toggleCompleteTask(task.id)}
+                  defaultChecked={task.completed}
+                />
               </div>
             ))}
           </>
