2f46742a28436fe6a310b4a7896211e47e35267e
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-02-26 04:04:02 -0800

    display tasks

--- client/src/App.js
+++ client/src/App.js
@@ -9,23 +9,25 @@ const App = () => {
   const [loading, setLoading] = useState(true);
   const address = "0x503BB3a1730f91F674970D8cA18Ef8d0b6EbC70C";
 
-  
-
   const loadBlockchainData = async () => {
     const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
-    const accounts = await web3.eth.getAccounts()
-    setAccount(accounts[0])
-    const todoList = new web3.eth.Contract(TodoList.abi,address)
-    console.log(todoList)
+    const accounts = await web3.eth.getAccounts();
+    setAccount(accounts[0]);
+    const todoList = new web3.eth.Contract(TodoList.abi, address);
+    console.log(todoList);
     const taskCount = await todoList.methods.taskCount().call();
-    setTaskCount(taskCount)
-    for(var i=1;i<= taskCount;i++){
+    setTaskCount(taskCount);
+    for (var i = 1; i <= taskCount; i++) {
       const task = await todoList.methods.tasks(i).call();
       console.log(task);
-      setTasks([...tasks,task])
+      setTasks([...tasks, task]);
     }
-    setLoading(false)
-  }
+    setLoading(false);
+  };
+
+  tasks.map(task => {
+    console.log(task.content);
+  })
 
   useEffect(() => {
     loadBlockchainData();
@@ -38,7 +40,16 @@ const App = () => {
           <div>loading..</div>
         ) : (
           <>
-            
+            <form>
+              <input type={"text"} placeholder="todo..." />
+              <button> Submit </button>
+            </form>
+            {tasks.map((task) => (
+              <div key={task.id}>
+                <label>{task.content}</label>
+                <input type={'checkbox'}  />
+              </div>
+            ))}
           </>
         )}
       </div>
--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.21 <8.10.0;
+pragma solidity >=0.4.21 <0.7.0;
 
 contract TodoList {
     uint256 public taskCount = 0;
@@ -12,22 +12,20 @@ contract TodoList {
 
     mapping(uint256 => Task) public tasks;
 
-    // Defined events
-    event TaskCreated(uint256 id, string content, bool completed);
-
+    event TaskCreated(uint256 id, string countent, bool completed);
     event TaskCompleted(uint256 id, bool completed);
 
     constructor() public {
-        createTask("Check out dappuniversity.com");
+        createTask("start learning blockchain");
     }
 
     function createTask(string memory _content) public {
-        taskCount++;
-        tasks[taskCount] = Task(taskCount, _content, false);
-        emit TaskCreated(taskCount, _content, false);
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount,_content,false);
+        emit TaskCreated(taskCount, _content,false);
     }
 
-    function toggleCompleted(uint256 _id) public {
+    function toggleCompleted(uint _id) public {
         Task memory _task = tasks[_id];
         _task.completed = !_task.completed;
         tasks[_id] = _task;
