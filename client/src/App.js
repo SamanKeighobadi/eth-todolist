22038ab5555c3f101c16a30fc7d4c5150d9dfbfc
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-02-25 21:54:23 -0800

    init states and variables

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
-import React, { Component } from "react";
+import React, { useState,useEffect } from "react";
 import "./App.css";
 import Web3 from "web3";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello world!</h1>
-      </div>
-    );
-  }
+const App = () => {
+
+  const [account,setAccount] = useState('');
+  const [taskCount,setTaskCount] = useState(0);
+  const [tasks,setTasks] = useState([]);
+  const [loading,setLoading] = useState(true)
+  const address = '';
+
+  return (
+    <div>Hello Word</div>
+  )
 }
 
 export default App;
