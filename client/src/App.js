372b0b7c911a6a00e8ec9aa80bf956e00eee5138
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-02-25 22:02:26 -0800

    feat: load blockchain
    access to the blockchain from frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState,useEffect } from "react";
 import "./App.css";
 import Web3 from "web3";
+import TodoList from './contracts/TodoList.json'
 
 const App = () => {
 
@@ -9,6 +10,22 @@ const App = () => {
   const [tasks,setTasks] = useState([]);
   const [loading,setLoading] = useState(true)
   const address = '';
+
+  const loadBlockchainData = () =>{
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    const accounts = await web3.eth.getAccounts()
+    setAccount(accounts[0])
+    const todoList = new web3.eth.Contract(TodoList.abi,address)
+    console.log(todoList)
+    const taskCount = await todoList.methods.taskCount().call();
+    setTaskCount(taskCount)
+
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  },[])
 
   return (
     <div>Hello Word</div>
