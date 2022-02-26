d6d7678a7773113351bb730eb1423a1d63c9a2b6
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-02-26 03:48:25 -0800

    fix:  mistake at contract address
    i was use account address instance contract address

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import React, { useState,useEffect } from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
 import Web3 from "web3";
-import TodoList from './contracts/TodoList.json'
+import TodoList from "./contracts/TodoList.json";
 
 const App = () => {
+  const [account, setAccount] = useState("");
+  const [taskCount, setTaskCount] = useState(0);
+  const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const address = "0x503BB3a1730f91F674970D8cA18Ef8d0b6EbC70C";
 
-  const [account,setAccount] = useState('');
-  const [taskCount,setTaskCount] = useState(0);
-  const [tasks,setTasks] = useState([]);
-  const [loading,setLoading] = useState(true)
-  const address = '';
+  
 
-  const loadBlockchainData = () =>{
+  const loadBlockchainData = async () => {
     const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
     const accounts = await web3.eth.getAccounts()
     setAccount(accounts[0])
@@ -19,17 +19,31 @@ const App = () => {
     console.log(todoList)
     const taskCount = await todoList.methods.taskCount().call();
     setTaskCount(taskCount)
-
+    for(var i=1;i<= taskCount;i++){
+      const task = await todoList.methods.tasks(i).call();
+      console.log(task);
+      setTasks([...tasks,task])
+    }
     setLoading(false)
   }
 
   useEffect(() => {
-    loadBlockchainData()
-  },[])
+    loadBlockchainData();
+  }, []);
 
   return (
-    <div>Hello Word</div>
-  )
-}
+    <>
+      <div>
+        {loading ? (
+          <div>loading..</div>
+        ) : (
+          <>
+            
+          </>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default App;
